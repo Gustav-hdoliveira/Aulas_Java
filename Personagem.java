6af135f77390050dc86d6a5d04c16927080410d0
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-11-14 21:49:27 -0300

    Add files via upload

--- Personagem.java
+++ Personagem.java
@@ -2,12 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package videogame;
+package Simulador_RPG;
 
 /**
  *
  * @author GUSTAVOHENRIQUEDEOLI
  */
 public class Personagem {
-    
+    String nome;
+    int altura;
+    int peso;
+    int level;
+    int vidaMAX;
+    int vidaAtual;
+    int almas;
+    String classeInicial;
+    int cargaMAXkg;
+    int PTvigor;
+    int PTresistencia;
+    int PTvitalidade;
+    int PTsintonização;
+    int PTforça;
+    int PTdestreza;
+    int PTadaptabilidade;
+    int PTinteligencia;
+    int PTfé;
+    String equipMdireita;
+    String equipMesquerda;
 }
